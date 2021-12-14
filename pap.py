1a90592337afadc9750fdfb162c1eaaff0f10a56
Timothee Carayol <timothee.carayol@gmail.com>
2021-12-14 11:12:56 +0100

    Update pap.py

--- pap.py
+++ pap.py
@@ -1,6 +1,6 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://www.paprikaapp.com/api/v1/sync/groceries/', auth=('timothee.carayol@gmail.com', 'dR2YpnbYvE5m'))
+    r = requests.get('https://www.paprikaapp.com/api/v1/sync/groceries/', auth=('EMAIL', 'PASSWORD'))
     print(r.status_code)
     print(r.text)
